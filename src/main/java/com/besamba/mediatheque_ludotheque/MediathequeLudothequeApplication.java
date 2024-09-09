966cc11be95f734eb80b68c3fb460582e9420d9e
LAZA-GLORIA <gloria.besamba@gmail.com>
2024-09-09 12:31:28 +0200

    initialisation service back

---
+++ src/main/java/com/besamba/mediatheque_ludotheque/MediathequeLudothequeApplication.java
@@ -0,0 +1,13 @@
+package com.besamba.mediatheque_ludotheque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediathequeLudothequeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediathequeLudothequeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/besamba/mediatheque_ludotheque/MediathequeLudothequeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.besamba.mediatheque_ludotheque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediathequeLudothequeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
